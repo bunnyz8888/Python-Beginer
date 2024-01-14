de7b4a50f49a431089b780182c81316cea297c01
Bang <151416686+bunnyz8888@users.noreply.github.com>
2024-01-14 09:06:25 +0700

    Add files via upload

---
+++ List.py
@@ -0,0 +1,13 @@
+#LIST 1
+#Bài 1: Đáp án c d
+#Bài 2: Không vì List có thể thay đổi nội dung
+#Bài 3: 
+s = 'aaaaaaaAAAAAaaa//123123//000000//&&TTT%%abcxyznontqfadf'
+print(s.split('&&')[-1].split('%%')[0])
+
+#LIST 1
+#Bài 1: rỗng
+lst = list()
+lst.pop
+print(lst)
+#Bài 2: Không vì không thể so sánh 1 chuỗi và 1 
